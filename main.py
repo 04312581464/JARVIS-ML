f573dc3dd607ded170765928c9a1bccafeadd7a3
Sindbad <88231729+04312581464@users.noreply.github.com>
2024-03-22 12:53:24 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from func.chat import Chat
+from func.SpeakOnline import Speak
+from func.ListenJs import Listen
+from func.DataOnline import Online_Scraper
+
+if __name__=="__main__":
+    while 1:
+        Q=Listen()
+        QL=Q.lower()
+        GetChat=Chat(QL)
+        GetData=Online_Scraper(Q)
+        if GetData != None:
+            Speak(GetData)
+        else:
+            Speak(GetChat[0])
